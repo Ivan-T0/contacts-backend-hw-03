73422c6b84c860b740200c95f65b0550556e1154
Иван-Т0 <vanyataran24@gmail.com>
2023-10-15 21:28:07 +0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
-
+const BD_HOST = require("./config")
 const mongoose = require("mongoose");
-const BD_HOST = "mongodb+srv://Ivan:L6IE1nbQpfP448Hf@cluster0.arvz5tf.mongodb.net/?retryWrites=true&w=majority"
+
 
 // mongoose.set('stringQuery', true)
 mongoose.connect(BD_HOST).then(() => {
