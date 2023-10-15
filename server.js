db0569ead8ccba4e17f70f2a8a13143cd4d3eeb2
Иван-Т0 <vanyataran24@gmail.com>
2023-10-15 21:44:58 +0300

    update

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const app = require('./app')
 const {BD_HOST} = process.env
 const mongoose = require("mongoose");
 
-console.log()
-// mongoose.set('stringQuery', true)
+
 mongoose.connect(BD_HOST).then(() => {
   app.listen(3000)
 }).catch(error => {
